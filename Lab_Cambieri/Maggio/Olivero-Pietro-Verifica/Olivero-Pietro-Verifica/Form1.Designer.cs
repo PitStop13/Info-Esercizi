4f771860266de63f9df99f2deca6414181971e01
PitStop13 <p.olivero.3584@vallauri.edu>
2025-05-21 07:44:59 +0200

    Aggiunta della verifica e modifica di alcuni file con pari mancanti

---
+++ Lab_Cambieri/Maggio/Olivero-Pietro-Verifica/Olivero-Pietro-Verifica/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Olivero_Pietro_Verifica
+{
+    partial class Verifica
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCarica = new System.Windows.Forms.Button();
+            this.btnSalva = new System.Windows.Forms.Button();
+            this.btnCercaPerRegistra = new System.Windows.Forms.Button();
+            this.btnFiltraPerAnno = new System.Windows.Forms.Button();
+            this.btnCalcolaMedia = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.txtRegista = new System.Windows.Forms.TextBox();
+            this.txtAnnoMassimo = new System.Windows.Forms.TextBox();
+            this.txtAnnoMinimo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelAnnoMIn = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCarica
+            // 
+            this.btnCarica.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarica.Location = new System.Drawing.Point(12, 12);
+            this.btnCarica.Name = "btnCarica";
+            this.btnCarica.Size = new System.Drawing.Size(167, 38);
+            this.btnCarica.TabIndex = 0;
+            this.btnCarica.Text = "Carica File";
+            this.btnCarica.UseVisualStyleBackColor = true;
+            this.btnCarica.Click += new System.EventHandler(this.btnCarica_Click);
+            // 
+            // btnSalva
+            // 
+            this.btnSalva.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalva.Location = new System.Drawing.Point(12, 87);
+            this.btnSalva.Name = "btnSalva";
+            this.btnSalva.Size = new System.Drawing.Size(167, 38);
+            this.btnSalva.TabIndex = 1;
+            this.btnSalva.Text = "Salva File";
+            this.btnSalva.UseVisualStyleBackColor = true;
+            this.btnSalva.Click += new System.EventHandler(this.btnSalva_Click);
+            // 
+            // btnCercaPerRegistra
+            // 
+            this.btnCercaPerRegistra.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCercaPerRegistra.Location = new System.Drawing.Point(12, 162);
+            this.btnCercaPerRegistra.Name = "btnCercaPerRegistra";
+            this.btnCercaPerRegistra.Size = new System.Drawing.Size(167, 38);
+            this.btnCercaPerRegistra.TabIndex = 2;
+            this.btnCercaPerRegistra.Text = "Cerca per Regista";
+            this.btnCercaPerRegistra.UseVisualStyleBackColor = true;
+            this.btnCercaPerRegistra.Click += new System.EventHandler(this.btnCercaPerRegistra_Click);
+            // 
+            // btnFiltraPerAnno
+            // 
+            this.btnFiltraPerAnno.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFiltraPerAnno.Location = new System.Drawing.Point(12, 237);
+            this.btnFiltraPerAnno.Name = "btnFiltraPerAnno";
+            this.btnFiltraPerAnno.Size = new System.Drawing.Size(167, 38);
+            this.btnFiltraPerAnno.TabIndex = 3;
+            this.btnFiltraPerAnno.Text = "Fitra per anno";
+            this.btnFiltraPerAnno.UseVisualStyleBackColor = true;
+            this.btnFiltraPerAnno.Click += new System.EventHandler(this.btnFiltraPerAnno_Click);
+            // 
+            // btnCalcolaMedia
+            // 
+            this.btnCalcolaMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcolaMedia.Location = new System.Drawing.Point(12, 312);
+            this.btnCalcolaMedia.Name = "btnCalcolaMedia";
+            this.btnCalcolaMedia.Size = new System.Drawing.Size(167, 38);
+            this.btnCalcolaMedia.TabIndex = 4;
+            this.btnCalcolaMedia.Text = "Calcola media voti";
+            this.btnCalcolaMedia.UseVisualStyleBackColor = true;
+            this.btnCalcolaMedia.Click += new System.EventHandler(this.btnCalcolaMedia_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToResizeColumns = false;
+            this.dgv.AllowUserToResizeRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(217, 12);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(560, 338);
+            this.dgv.TabIndex = 5;
+            // 
+            // txtRegista
+            // 
+            this.txtRegista.Location = new System.Drawing.Point(37, 402);
+            this.txtRegista.Name = "txtRegista";
+            this.txtRegista.Size = new System.Drawing.Size(166, 20);
+            this.txtRegista.TabIndex = 6;
+            // 
+            // txtAnnoMassimo
+            // 
+            this.txtAnnoMassimo.Location = new System.Drawing.Point(611, 402);
+            this.txtAnnoMassimo.Name = "txtAnnoMassimo";
+            this.txtAnnoMassimo.Size = new System.Drawing.Size(166, 20);
+            this.txtAnnoMassimo.TabIndex = 7;
+            // 
+            // txtAnnoMinimo
+            // 
+            this.txtAnnoMinimo.Location = new System.Drawing.Point(412, 402);
+            this.txtAnnoMinimo.Name = "txtAnnoMinimo";
+            this.txtAnnoMinimo.Size = new System.Drawing.Size(166, 20);
+            this.txtAnnoMinimo.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 378);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Inserire il nome del regista (CASE SENSITIVE)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(660, 378);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Anno massimo";
+            // 
+            // labelAnnoMIn
+            // 
+            this.labelAnnoMIn.AutoSize = true;
+            this.labelAnnoMIn.Location = new System.Drawing.Point(464, 378);
+            this.labelAnnoMIn.Name = "labelAnnoMIn";
+            this.labelAnnoMIn.Size = new System.Drawing.Size(70, 13);
+            this.labelAnnoMIn.TabIndex = 11;
+            this.labelAnnoMIn.Text = "Anno minimo:";
+            // 
+            // Verifica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(800, 434);
+            this.Controls.Add(this.labelAnnoMIn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAnnoMinimo);
+            this.Controls.Add(this.txtAnnoMassimo);
+            this.Controls.Add(this.txtRegista);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btnCalcolaMedia);
+            this.Controls.Add(this.btnFiltraPerAnno);
+            this.Controls.Add(this.btnCercaPerRegistra);
+            this.Controls.Add(this.btnSalva);
+            this.Controls.Add(this.btnCarica);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Verifica";
+            this.Text = "Verifica Olivero";
+            this.Load += new System.EventHandler(this.Verifica_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCarica;
+        private System.Windows.Forms.Button btnSalva;
+        private System.Windows.Forms.Button btnCercaPerRegistra;
+        private System.Windows.Forms.Button btnFiltraPerAnno;
+        private System.Windows.Forms.Button btnCalcolaMedia;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.TextBox txtRegista;
+        private System.Windows.Forms.TextBox txtAnnoMassimo;
+        private System.Windows.Forms.TextBox txtAnnoMinimo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelAnnoMIn;
+    }
+}
+
---
+++ Lab_Cambieri/Maggio/Olivero-Pietro-Verifica/Olivero-Pietro-Verifica/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Olivero_Pietro_Verifica
+{
+    public partial class Verifica : Form
+    {
+        struct Film
+        {
+            public string Titolo;
+            public string Regista;
+            public int Anno;
+            public double Voto;
+        }
+        public Verifica()
+        {
+            InitializeComponent();
+        }
+        private void Verifica_Load(object sender, EventArgs e)
+        {
+            SettaDgv();
+        }
+
+        private void SettaDgv()
+        {
+            dgv.AllowUserToAddRows = false; dgv.AllowUserToDeleteRows = false;
+            dgv.AllowUserToResizeColumns = false; dgv.AllowUserToResizeRows = false;
+            dgv.ReadOnly = false; dgv.RowHeadersVisible = false;
+            
+            dgv.ColumnCount = 4;
+            dgv.Columns[0].Name = "Titolo";
+            dgv.Columns[1].Name = "Regista";
+            dgv.Columns[2].Name = "Anno";
+            dgv.Columns[3].Name = "Voto";
+
+            dgv.Columns[0].Width = 150;
+            dgv.Columns[1].Width = 200;
+            dgv.Columns[2].Width = 50;
+            dgv.Columns[3].Width = 50;
+
+
+            dgv.Columns[0].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgv.Columns[1].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgv.Columns[2].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgv.Columns[3].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+        }
+
+        private void btnCarica_Click(object sender, EventArgs e)
+        {
+            
+            if (!File.Exists("filmoteca.csv"))
+            {
+                MessageBox.Show("Il file filmoteca.csv non esiste", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                dgv.Rows.Clear();
+                StreamReader sr = new StreamReader("filmoteca.csv");
+                string[] righe = File.ReadAllLines("filmoteca.csv");
+                for (int i = 0; i < righe.Length; i++)
+                {
+                    string[] dati = righe[i].Split(',');
+                    Film film = new Film();
+                    film.Titolo = dati[0];
+                    film.Regista = dati[1];
+                    film.Anno = int.Parse(dati[2]);
+                    film.Voto = double.Parse(dati[3].Replace('.', ','));
+                    dgv.Rows.Add(film.Titolo, film.Regista, film.Anno, film.Voto);
+                }
+                sr.Close();
+            }
+        }
+
+        private void btnSalva_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("filmoteca.csv");
+
+            foreach (DataGridViewRow riga in dgv.Rows)
+            {
+                if (riga.IsNewRow) continue;
+
+                string titolo = riga.Cells["Titolo"].Value?.ToString() ?? "";
+                string regista = riga.Cells["Regista"].Value?.ToString() ?? "";
+                string anno = riga.Cells["Anno"].Value?.ToString() ?? "";
+                string voto = riga.Cells["Voto"].Value?.ToString()?.Replace(',', '.') ?? "";
+
+                sw.WriteLine($"{titolo},{regista},{anno},{voto}");
+            }
+
+            sw.Close();
+
+            MessageBox.Show("File salvato correttamente.", "Salvataggio completato", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnCercaPerRegistra_Click(object sender, EventArgs e)
+        {
+            
+            string registaDaCercare = txtRegista.Text.Trim();
+
+            if (registaDaCercare == "")
+            {
+                MessageBox.Show("Inserisci un nome di regista da cercare.", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            List<string[]> filmTrovati = new List<string[]>();
+
+            foreach (DataGridViewRow riga in dgv.Rows)
+            {
+                if (riga.IsNewRow) continue;
+
+                string regista = riga.Cells["Regista"].Value?.ToString() ?? "";
+
+                if (regista.ToLower().Contains(registaDaCercare.ToLower()))
+                {
+                    string titolo = "";
+                    if (riga.Cells["Titolo"].Value != null)
+                        titolo = riga.Cells["Titolo"].Value.ToString();
+
+                    string anno = "";
+                    if (riga.Cells["Anno"].Value != null)
+                        anno = riga.Cells["Anno"].Value.ToString();
+
+                    string voto = "";
+                    if (riga.Cells["Voto"].Value != null)
+                        voto = riga.Cells["Voto"].Value.ToString();
+
+                    filmTrovati.Add(new string[] { titolo, regista, anno, voto });
+                }
+            }
+
+            dgv.Rows.Clear();
+
+            foreach (var film in filmTrovati)
+            {
+                dgv.Rows.Add(film[0], film[1], film[2], film[3]);
+            }
+
+            if (filmTrovati.Count == 0)
+            {
+                MessageBox.Show("Nessun film trovato per il regista inserito.", "Risultato", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnFiltraPerAnno_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtAnnoMinimo.Text.Trim(), out int annoMin) ||
+                !int.TryParse(txtAnnoMassimo.Text.Trim(), out int annoMax))
+            {
+                MessageBox.Show("Inserisci anni validi.", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (annoMin > annoMax)
+            {
+                MessageBox.Show("L'anno minimo deve essere minore o uguale all'anno massimo.", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            bool trovatoAlmenoUno = false;
+
+            
+            int righeTotali = dgv.Rows.Count;
+            string[,] dati = new string[righeTotali, 4];
+            int indice = 0;
+
+            foreach (DataGridViewRow riga in dgv.Rows)
+            {
+                if (riga.IsNewRow) continue;
+
+                if (int.TryParse(riga.Cells["Anno"].Value?.ToString(), out int anno) &&
+                    anno >= annoMin && anno <= annoMax)
+                {
+                    if (riga.Cells["Titolo"].Value != null)
+                        dati[indice, 0] = riga.Cells["Titolo"].Value.ToString();
+                    else
+                        dati[indice, 0] = "";
+
+                    if (riga.Cells["Regista"].Value != null)
+                        dati[indice, 1] = riga.Cells["Regista"].Value.ToString();
+                    else
+                        dati[indice, 1] = "";
+
+                    if (riga.Cells["Anno"].Value != null)
+                        dati[indice, 2] = riga.Cells["Anno"].Value.ToString();
+                    else
+                        dati[indice, 2] = "";
+
+                    if (riga.Cells["Voto"].Value != null)
+                        dati[indice, 3] = riga.Cells["Voto"].Value.ToString();
+                    else
+                        dati[indice, 3] = "";
+
+                    indice++;
+                    trovatoAlmenoUno = true;
+                }
+            }
+
+            
+            dgv.Rows.Clear();
+
+            for (int i = 0; i < indice; i++)
+            {
+                dgv.Rows.Add(dati[i, 0], dati[i, 1], dati[i, 2], dati[i, 3]);
+            }
+
+            if (!trovatoAlmenoUno)
+            {
+                MessageBox.Show("Nessun film trovato per gli anni inseriti.", "Risultato", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnCalcolaMedia_Click(object sender, EventArgs e)
+        {
+            
+            double sommaVoti = 0;
+
+            int numerodiFilm = 0;
+
+            foreach (DataGridViewRow riga in dgv.Rows)
+            {
+                if (!riga.IsNewRow && double.TryParse(riga.Cells["Voto"].Value?.ToString(), out double voto))
+                {
+                    sommaVoti += voto;
+                    numerodiFilm++;
+                }
+
+            }
+
+            if (numerodiFilm > 0)
+            {
+                double media = sommaVoti / numerodiFilm;
+                MessageBox.Show($"La media dei voti è: {media:F2}", "Media Voti", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Nessun voto trovato.", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
