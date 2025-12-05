583410aa9285ecd11d6173d2d1104beea88ac2f5
PitStop13 <p.olivero.3584@vallauri.edu>
2025-12-05 09:37:41 +0100

    Aggiunto database factory

---
+++ 2025-26/Novembre/Mock/MockTest_Solution/MockTest/DatabaseFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace MockTest
+{
+    public enum DataBaseType
+    {
+        /// <summary>
+        /// database basato su file 
+        /// database bassato su liste in memoria
+        /// </summary>
+        fileBase,
+        inmemory,
+    }
+
+    internal static class DataBaseFactory
+    {
+        public static IDatabaseMock CreateDataBase(DataBaseType tipo)
+        {
+            switch (tipo)
+            {
+                case DataBaseType.fileBase:
+                    return new DatabaseMock();
+
+                case DataBaseType.inmemory:
+                //return new ;
+                default:
+                    throw new ArgumentException($"tipo di database non supotato");
+            }
+        }
+
+        public static IDatabaseMock CreateDataBase()
+        {
+            return new DatabaseMock();
+        }
+    }
+}
+
+
+
+
+
+
