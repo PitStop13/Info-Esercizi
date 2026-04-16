e9cce4099f8644c8cb975df0f5575b5b8d860bd4
Pietro Olivero <p.olivero.3584@vallauri.edu>
2026-04-16 13:50:35 +0200

    Aggiornato es Thread

--- 2025-26/Marzo-Aprile/Thread/ThreadKitchen_Solution/ThreadKitchen/Chef.cs
+++ 2025-26/Marzo-Aprile/Thread/ThreadKitchen_Solution/ThreadKitchen/Chef.cs
@@ -1,32 +1,44 @@
-﻿    using System.Collections.Generic;
-    using System.Net.Http.Headers;
+﻿using System.Collections.Generic;
+using System.Net.Http.Headers;
 
-    namespace ThreadKitchen
+namespace ThreadKitchen
+{
+    public class Chef
     {
-        public class Chef
-        {
-            // Identificativo numerico del cuoco (0, 1, 2, 3)
-            public int Id { get; set; }
-            // Nome del cuoco
-            public string Name { get; set; }
-            // Nome del piatto che sta preparando
-            public string Dish { get; set; }
-            // Avanzamento da 0 a 100
-            public int Progress { get; set; }
-            // true quando il piatto e&#39; completato
-            public bool IsFinished { get; set; }
-            // Tempo totale impiegato in secondi
-            public double ElapsedSeconds { get; set; }
+        /// <summary> Identificativo numerico del cuoco (0, 1, 2, 3) </summary>
+        public int Id { get; set; }
 
-            public static List<Chef> GetDefaultChefs()
+        /// <summary> Nome del cuoco </summary>
+        public string Name { get; set; }
+
+        /// <summary> Nome del piatto che sta preparando </summary>
+        public string Dish { get; set; }
+
+        /// <summary> Avanzamento da 0 a 100 </summary>
+        public int Progress { get; set; }
+
+        /// <summary> true quando il piatto è completato </summary>
+        public bool IsFinished { get; set; }
+
+        /// <summary> Tempo totale impiegato in secondi </summary>
+        public double ElapsedSeconds { get; set; }
+
+        /// <summary> true quando il cuoco sta uitlizzando un fornello </summary>
+        public bool isUsingFornello { get; set; }
+
+        /// <summary>
+        /// Restitusice 4 cucochi predefiniti con nomi e piatti italiani
+        /// </summary>
+        /// <returns></returns>
+        public static List<Chef> GetDefaultChefs()
+        {
+            return new List<Chef>
             {
-                return new List<Chef>
-                {
-                    new Chef { Id = 0, Name = "Mario", Dish = "Lasagna" },
-                new Chef { Id = 1, Name = "Giulia", Dish = "Risotto ai funghi" },
-                new Chef { Id = 2, Name = "Luca", Dish = "Tiramisu" },
-                new Chef { Id = 3, Name = "Federica", Dish = "Ossobuco alla milanese" },
-                };
-            }
+                new Chef { Id = 0, Name = "Mario", Dish = "Lasagna", Progress = 0, IsFinished = false, isUsingFornello = false },
+                new Chef { Id = 1, Name = "Giulia", Dish = "Risotto ai funghi", Progress = 0, IsFinished = false, isUsingFornello = false },
+                new Chef { Id = 2, Name = "Luca", Dish = "Tiramisu", Progress = 0, IsFinished = false, isUsingFornello = false },
+                new Chef { Id = 3, Name = "Federica", Dish = "Ossobuco alla milanese", Progress = 0, IsFinished = false, isUsingFornello = false },
+            };
         }
     }
+}
